798ca4d335636dd0702f737dd0f855bb1ff76730
Nick Bielawski <nbielawski12@gmail.com>
2017-11-13 16:50:05 -0500

    first commit

---
+++ GuessingGame/Models/GameModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame.Models
+{
+    public class GameModel
+    {
+
+        [Required]
+        [MaxLength(25, ErrorMessage = "The maximum name length is 25 characters.")]
+        [Display(Name = "Player Name")]
+        public string PlayerName { get; set; }
+
+        [Range(1, 10, ErrorMessage ="Guess must be between 1 and 10")]
+        public int Guess { get; set; }
+    }
+}
